a295d13fba05d6a89b583f684f6749c05071b63b
Jason Zhai <vines15@gmail.com>
2013-09-28 22:19:07 -0700

    mix
    add sponsor tabs;
    add blog content;

--- tpl-slider.php
+++ tpl-slider.php
@@ -7,5 +7,5 @@
        	<?php 	while ( have_posts() ) : the_post(); ?>
         <?php 		get_template_part( 'framework/loops/content', 'page' ); ?>
         <?php 	endwhile;
-           	  endif;?>
+           	  endif;?>	  
 <?php get_footer();?>
