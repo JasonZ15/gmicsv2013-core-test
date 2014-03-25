c65a70785c3f38492ef052c75a3501bfa2b05f99
Jason Zhai <vines15@gmail.com>
2014-03-25 15:24:34 -0700

    add japanese to language menu

--- header.php
+++ header.php
@@ -117,7 +117,7 @@
                                   echo "	</a>";
                                   echo "</li>"; 						 
                                endforeach;?>
-                               <li class="chinese"><a href="http://beijing.thegmic.com/cn/"></a></li><li class="english"><a href="http://beijing.thegmic.com/"></a></li>
+                               <li class="chinese"><a href="http://beijing.thegmic.com/cn/"></a></li><li class="english"><a href="http://beijing.thegmic.com/"></a><li class="japanese"><a href="http://beijing.thegmic.com/jp/"></a></li>
                         </ul>
                 <?php endif;?>
 	<ul id="menu">
