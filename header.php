b84cd4a3e739558b92af93dac08500a62a121eb2
Darren <darren@greatwallclub.com>
2014-03-26 15:06:04 +0800

    added JP language link

--- header.php
+++ header.php
@@ -116,7 +116,7 @@
                                   echo "	</a>";
                                   echo "</li>"; 						 
                                endforeach;?>
-                               <li class="chinese"><a href="http://beijing.thegmic.com/cn/"></a></li><li class="english"><a href="http://beijing.thegmic.com/"></a></li>
+                              <li class="chinese"><a href="http://beijing.thegmic.com/cn/"></a></li><li class="english"><a href="http://beijing.thegmic.com/"></a></li><li class="japanese"><a href="http://beijing.thegmic.com/jp/"></a></li>
                         </ul>
                 <?php endif;?>
 	<ul id="menu">
